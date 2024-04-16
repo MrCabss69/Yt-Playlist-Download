9256908193b292db5e45a9fb64e49942cdd3e79c
MrCabss69 <p.pintor@alumnos.upm.es>
2024-04-16 16:56:46 +0200

    upgrade and refactor, allowed inputs as .txt

--- README.md
+++ README.md
@@ -1,44 +1,55 @@
+
 # Youtube Playlist Downloader
 
 This project allows you to download songs from **public** YouTube playlists in MP3 format and save them to a local folder. It uses the `pytube` library for downloading and `moviepy` for format conversion.
 
-##Requirements
+## Requirements
 
 Before you begin, make sure you have Python installed on your system. Additionally, you will need to install the following libraries:
 
 - pytube
-- movie
+- moviepy
 
 You can install these dependencies with the following command:
 
-```tap
+```bash
 pip install pytube moviepy
 ```
 
-Finally, navigate to the playlist you want to download, and copy the id of the playlist you want to download (what appears to the right of list=)
-
-
-
-
 ## Use
 
-To use this script, follow these steps:
+This script supports downloading from a single playlist, multiple playlists by specifying comma-separated IDs, or from a list of playlist IDs provided in a text file. Follow these steps to use the script:
 
 1. Clone this repository locally.
+```bash
+git clone https://github.com/MrCabss69/Yt-Playlist-Download.git
+```
+1. Open a terminal or command line and navigate to the directory where the script is located.
+```bash
+cd  Yt-Playlist-Download
+```
+2. Run the script by providing the playlist ID(s) or the path to a text file containing playlist IDs. You can also specify the output directory where the MP3 files will be saved.
 
-2. Open a terminal or command line.
-
-3. Navigate to the directory where the script is located.
-
-4. Run the script with the YouTube playlist ID as an argument:
-
-```tap
-python3 download_playlist.py playlist_id --output output_directory
+### Single Playlist
+```bash
+python3 download_playlist.py ID_1 --output output_directory
 ```
 
-Replace `[playlist ID]` with the actual ID of the YouTube playlist you want to download.
+### Multiple Playlists
+```bash
+python3 download_playlist.py ID_1,ID_2 --output output_directory
+```
+
+### Playlists from Text File
+```bash
+python3 download_playlist.py playlists.txt --output output_directory
+```
 
 ## Example
-```tap
-python3 download_playlist.py playlist_id --output output_directory
+To download a single YouTube playlist and save the MP3 files to a specified folder:
+
+```bash
+python3 download_playlist.py ID_1 --output music_folder
 ```
+
+Replace `ID_1`,`ID_2`  with the actual ID of the YouTube PUBLIC (this is a must) playlist you want to download. For multiple IDs or a file, adjust the input accordingly.
--- download_playlist.py
+++ download_playlist.py
@@ -4,32 +4,49 @@ from pytube import Playlist
 from moviepy.editor import AudioFileClip
 
 def download_and_convert(playlist_url, output_directory):
+    try:
+        pl = Playlist(playlist_url)
+        for video in pl.videos:
+            try:
+                # Descarga el stream de audio
+                audio_stream = video.streams.filter(only_audio=True).first()
+                audio_file = audio_stream.download(output_path=output_directory)
 
-    pl = Playlist(playlist_url)
-    for video in pl.videos:
-        try:
-            # Descarga el stream de audio
-            audio_stream = video.streams.filter(only_audio=True).first()
-            audio_file = audio_stream.download(output_path=output_directory)
+                # Nombre del archivo final
+                final_file = os.path.splitext(audio_file)[0] + '.mp3'
 
-            # Nombre del archivo final
-            final_file = os.path.splitext(audio_file)[0] + '.mp3'
+                # Convertir el archivo a MP3
+                with AudioFileClip(audio_file) as audio_clip:
+                    audio_clip.write_audiofile(final_file)
 
-            # Convertir el archivo a MP3
-            with AudioFileClip(audio_file) as audio_clip:
-                audio_clip.write_audiofile(final_file)
+                # Eliminar el archivo original descargado
+                os.remove(audio_file)
 
-            # Eliminar el archivo original descargado
-            os.remove(audio_file)
+                print(f'Descargado y convertido: {video.title}')
+            except Exception as e:
+                print(f'Error al descargar o convertir {video.title}: {e}')
+    except Exception as e:
+        print(f'Error al procesar la playlist {playlist_url}: {e}')
 
-            print(f'Descargado y convertido: {video.title}')
-        except Exception as e:
-            print(f'Error al descargar o convertir {video.title}: {e}')
+def process_input(input_value, output_directory):
+    if os.path.isfile(input_value):
+        with open(input_value, 'r') as file:
+            playlist_ids = [line.strip() for line in file]
+    else:
+        playlist_ids = [id.strip() for id in input_value.split(',')]
+
+    for playlist_id in playlist_ids:
+        playlist_url = f'https://www.youtube.com/playlist?list={playlist_id}'
+        download_and_convert(playlist_url, output_directory)
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Descarga y convierte vídeos de YouTube a MP3.')
-    parser.add_argument('playlist_id', help='ID de la lista de reproducción de YouTube')
-    parser.add_argument('--output', help='Directorio de salida para los archivos MP3')
+    parser.add_argument('input', help='ID de lista de reproducción, lista de IDs separada por comas, o archivo de texto con IDs.')
+    parser.add_argument('--output', help='Directorio de salida para los archivos MP3', default='./')
     args = parser.parse_args()
-    playlist_url = f'https://www.youtube.com/playlist?list={args.playlist_id}'
-    download_and_convert(playlist_url, args.output)
+    
+    # Procesar los inputs y manejar los errores adecuadamente
+    try:
+        process_input(args.input, args.output)
+    except Exception as e:
+        print(f'Error al procesar el input: {e}')
